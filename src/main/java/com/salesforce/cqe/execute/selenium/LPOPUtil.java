3dd65eb0ff320e37806ea0aa68a418b2123849c3
gneumann2 <gneumann2@users.noreply.github.com>
2019-08-10 15:20:18 +0200

    Added method to find element recursively inside multiple shadowRoot's.

--- src/main/java/com/salesforce/cqe/execute/selenium/LPOPUtil.java
+++ src/main/java/com/salesforce/cqe/execute/selenium/LPOPUtil.java
@@ -3,22 +3,37 @@
  */
 package com.salesforce.cqe.execute.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.salesforce.lpop.webutils.impl.seti.ShadowRootWebElement;
 
 /**
- * Methods copy-pasted from BaseWebDriverUtil in core.
+ * Methods copy-pasted from BaseWebDriverUtil in core and augmented with code in Quip:
+ * "WebDriver + Shadow DOM traversal code samples" (https://salesforce.quip.com/sXNZAZtvgCZW)
  */
 public class LPOPUtil {
-	public static Object executeScript(WebDriver driver, String script, Object... parameters) {
-		return ((JavascriptExecutor) driver).executeScript(script, parameters);
+	public final static WebElement findElementInShadowRoot(WebDriver driver, WebElement rootElement, By... selectors) {
+	    WebElement curr = expandShadowIfExists(driver, rootElement);
+	    for (int i = 0; i < selectors.length; i++) {
+	        curr = curr.findElement(selectors[i]);
+	        if (i != selectors.length -1) {
+	            curr = expandShadowIfExists(driver, curr);
+	        }
+	    }
+	    return curr;
 	}
 
-	public static String getCheckShadowRootScript() {
+	public final static WebElement expandShadowIfExists(WebDriver driver, WebElement sc) {
+		if (sc instanceof WebElement && Boolean.TRUE.equals(executeScript(driver, getCheckShadowRootScript(), sc))) {
+			return new ShadowRootWebElement(sc);
+		}
+		return sc;
+	}
+
+	private static String getCheckShadowRootScript() {
 		StringBuilder scriptBuilder = new StringBuilder("var shadowRoot = arguments[0].shadowRoot;");
 		scriptBuilder.append("if (shadowRoot && shadowRoot.nodeType === 11 && !!shadowRoot.host) {");
 		scriptBuilder.append("    return true;");
@@ -27,10 +42,7 @@ public class LPOPUtil {
 		return scriptBuilder.toString();
 	}
 
-	public final static SearchContext expandShadowIfExists(WebDriver driver, SearchContext sc) {
-		if (sc instanceof WebElement && Boolean.TRUE.equals(executeScript(driver, getCheckShadowRootScript(), sc))) {
-			return new ShadowRootWebElement((WebElement) sc);
-		}
-		return sc;
+	private static Object executeScript(WebDriver driver, String script, Object... parameters) {
+		return ((JavascriptExecutor) driver).executeScript(script, parameters);
 	}
 }
