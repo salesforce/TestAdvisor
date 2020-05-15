9b0782d36ec002e6493a430f38cbf85459a0719d
Georg Neumann <gneumann@salesforce.com>
2020-05-15 18:10:05 +0200

    Adapt to change of SauceLabs Concurrency API.

--- src/main/java/com/salesforce/cqe/common/pojo/SaucelabsVMConcurrencyResponse.java
+++ src/main/java/com/salesforce/cqe/common/pojo/SaucelabsVMConcurrencyResponse.java
@@ -3,7 +3,6 @@ package com.salesforce.cqe.common.pojo;
 import lombok.Getter;
 import lombok.Setter;
 
-
 /*
 class to map response from saucelabs concurrency api: "https://saucelabs.com/rest/v1.2/users/" + <saucelabs username> + "/concurrency";
 example of response: {
@@ -22,50 +21,64 @@ example of response: {
                 "mac_vms": 15
             }
         },
-        "team": {}
+        "team": {
+			"current": {
+				"vms": 1,
+				"rds": 0,
+				"mac_vms": 0
+			},
+			"id": "f014921fe520453db0f45519a59ed763",
+			"allowed": {
+				"vms": 15,
+				"rds": 0,
+				"mac_vms": 15
+			}
+        }
     }
 }
  */
 @Getter
 @Setter
 public class SaucelabsVMConcurrencyResponse {
-    public double timestamp;
-    public Concurrency concurrency;
+	public double timestamp;
+	public Concurrency concurrency;
 
-    @Getter
-    @Setter
-    public static class Concurrency{
-        private Organization organization;
-        private Team team;
-    }
+	@Getter
+	@Setter
+	public static class Concurrency {
+		private Organization organization;
+		private Team team;
+	}
 
-    @Getter
-    @Setter
-    public static class Team{
+	@Getter
+	@Setter
+	public static class Team {
+		private Current current;
+		private Allowed allowed;
+		private String id;
+	}
 
-    }
+	@Getter
+	@Setter
+	public static class Organization {
+		private Current current;
+		private Allowed allowed;
+		private String id;
+	}
 
-    @Getter
-    @Setter
-    public static class Organization{
-        private Current current;
-        private Allowed allowed;
-        private String id;
-    }
+	@Getter
+	@Setter
+	public static class Current {
+		private int vms;
+		private int rds;
+		private int mac_vms;
+	}
 
-    @Getter
-    @Setter
-    public static class Current{
-        private int vms;
-        private int rds;
-        private int mac_vms;
-    }
-
-    @Getter
-    @Setter
-    public static class Allowed{
-        private int vms;
-        private int rds;
-        private int mac_vms;
-    }
+	@Getter
+	@Setter
+	public static class Allowed {
+		private int vms;
+		private int rds;
+		private int mac_vms;
+	}
 }
