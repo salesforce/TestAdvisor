6e85cfb4a479237514af969df7f386ef8227833f
Yibing Tao <ytao@salesforce.com>
2021-12-17 11:08:18 -0800

    @w-10273658 add RC install scripts

---
+++ scripts/m2-install.bat
@@ -0,0 +1,13 @@
+@echo off
+if [%1]==[] goto :usage
+
+echo "Installing testadvisor-lib-%1"
+call mvn install:install-file -Dfile=testadvisor-lib-%1.pom -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib -Dversion=%1 -Dpackaging=pom
+
+call mvn install:install-file -Dfile=testadvisor-lib-%1.jar -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib -Dsources=testadvisor-lib-%1-sources.jar -Djavadoc=testadvisor-lib-%1-javadoc.jar -Dversion=%1 -Dpackaging=jar 
+goto :done
+
+:usage
+echo m2-install.bat version
+
+:done
