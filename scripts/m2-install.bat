bb835b0bae495aa386abb99aa9d6b516e8e3ea8e
Georg Neumann <gneumann@salesforce.com>
2022-01-05 14:58:23 +0100

    Better m2-install script for Windows

--- scripts/m2-install.bat
+++ scripts/m2-install.bat
@@ -1,23 +1,18 @@
 @echo off
-if [%1]==[] goto :usage
 
-echo "Installing testadvisor-lib-%1"
-call mvn install:install-file -Dfile=testadvisor-lib-%1.pom -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib -Dversion=%1 -Dpackaging=pom
+set VERSION=0.1.17
 
-call mvn install:install-file -Dfile=testadvisor-lib-%1.jar -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib -Dsources=testadvisor-lib-%1-sources.jar -Djavadoc=testadvisor-lib-%1-javadoc.jar -Dversion=%1 -Dpackaging=jar 
+echo "Installing testadvisor-lib-%VERSION%"
+call mvn install:install-file -Dfile=testadvisor-lib-%VERSION%.pom -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib -Dversion=%VERSION% -Dpackaging=pom
 
-echo "Installing testadvisor-lib-selenium-3-%1"
-call mvn install:install-file -Dfile=testadvisor-lib-selenium-3-%1.pom -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib-selenium-3 -Dversion=%1 -Dpackaging=pom
+call mvn install:install-file -Dfile=testadvisor-lib-%VERSION%.jar -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib -Dsources=testadvisor-lib-%VERSION%-sources.jar -Djavadoc=testadvisor-lib-%VERSION%-javadoc.jar -Dversion=%VERSION% -Dpackaging=jar 
 
-call mvn install:install-file -Dfile=testadvisor-lib-selenium-3-%1.jar -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib-selenium-3 -Dsources=testadvisor-lib-selenium-3-%1-sources.jar -Djavadoc=testadvisor-lib-selenium-3-%1-javadoc.jar -Dversion=%1 -Dpackaging=jar 
+echo "Installing testadvisor-lib-selenium-3-%VERSION%"
+call mvn install:install-file -Dfile=testadvisor-lib-selenium-3-%VERSION%.pom -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib-selenium-3 -Dversion=%VERSION% -Dpackaging=pom
 
-echo "Installing testadvisor-lib-selenium-4-%1"
-call mvn install:install-file -Dfile=testadvisor-lib-selenium-4-%1.pom -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib-selenium-4 -Dversion=%1 -Dpackaging=pom
+call mvn install:install-file -Dfile=testadvisor-lib-selenium-3-%VERSION%.jar -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib-selenium-3 -Dsources=testadvisor-lib-selenium-3-%VERSION%-sources.jar -Djavadoc=testadvisor-lib-selenium-3-%VERSION%-javadoc.jar -Dversion=%VERSION% -Dpackaging=jar 
 
-call mvn install:install-file -Dfile=testadvisor-lib-selenium-4-%1.jar -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib-selenium-4 -Dsources=testadvisor-lib-selenium-4-%1-sources.jar -Djavadoc=testadvisor-lib-selenium-4-%1-javadoc.jar -Dversion=%1 -Dpackaging=jar 
-goto :done
+echo "Installing testadvisor-lib-selenium-4-%VERSION%"
+call mvn install:install-file -Dfile=testadvisor-lib-selenium-4-%VERSION%.pom -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib-selenium-4 -Dversion=%VERSION% -Dpackaging=pom
 
-:usage
-echo m2-install.bat version
-
-:done 
+call mvn install:install-file -Dfile=testadvisor-lib-selenium-4-%VERSION%.jar -DgroupId=com.salesforce.cte -DartifactId=testadvisor-lib-selenium-4 -Dsources=testadvisor-lib-selenium-4-%VERSION%-sources.jar -Djavadoc=testadvisor-lib-selenium-4-%VERSION%-javadoc.jar -Dversion=%VERSION% -Dpackaging=jar 
